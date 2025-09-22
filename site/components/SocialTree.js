ddb296ba940ed0b56e0ee7883c56614594a885ef
Cole Sladowsky <coleslad31@gmail.com>
2025-09-22 16:35:21 -0400

    link again  fix

--- site/components/SocialTree.js
+++ site/components/SocialTree.js
@@ -18,22 +18,22 @@ useEffect(() => {
   return (
     <>
       <div className="social flex flex-row justify-center">
-        {(facebook !== undefined )&& <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://facebook.com/${facebook}`}>
+        {(facebook !== undefined && facebook !== null && facebook !== '')&& <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://facebook.com/${facebook}`}>
           <img src="/svg/logos/facebook.svg" className='w-6'/>
         </Link>}
-        {(instagram !== undefined) && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://instagram.com/${instagram}`}>
+        {(instagram !== undefined && instagram !== null && instagram !== '') && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://instagram.com/${instagram}`}>
           <img src="/svg/logos/instagram.svg" className='w-6'/>
         </Link>}
-        {(twitter !== undefined) && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://twitter.com/${twitter}`}>
+        {(twitter !== undefined && twitter !== null && twitter !== '') && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://twitter.com/${twitter}`}>
           <img src="/svg/logos/twitter.svg" className='w-6'/>
         </Link>}
-        {(youtube !== undefined) && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://youtube.com/${youtube}`}>
+        {(youtube !== undefined && youtube !== null && youtube !== '') && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://youtube.com/${youtube}`}>
           <img src="/svg/logos/youtube.svg" className='w-6'/>
         </Link>}
-        {(github !== undefined) && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://github.com/${github}`}>
+        {(github !== undefined && github !== null && github !== '') && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://github.com/${github}`}>
           <img src="/svg/logos/github.svg" className='w-6'/>
         </Link>}
-        {(linkedin !== undefined) && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://linkedin.com/${linkedin}`}>
+        {(linkedin !== undefined && linkedin !== null && linkedin !== '') && <Link className='bg-cyan-950 bg-opacity-30 rounded-full p-2 hover:bg-cyan-600 hover:bg-opacity-25 transition-all duration-600 hover:scale-110 border border-gray-400 mx-1 my-4 select-none' target="_blank" href={`https://linkedin.com/${linkedin}`}>
           <img src="/svg/logos/linkedin.svg" className='w-6'/>
         </Link>}
         
