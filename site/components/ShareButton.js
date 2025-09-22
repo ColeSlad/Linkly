60dc540ba50bf378825f11333474cb4d3f7e6b6d
Cole Sladowsky <coleslad31@gmail.com>
2025-09-22 18:22:41 -0400

    fix copy

--- site/components/ShareButton.js
+++ site/components/ShareButton.js
@@ -7,7 +7,7 @@ const ShareButton = () => {
   const router = useRouter();
 
   const copyLink = () => {
-    navigator.clipboard.writeText(`/${router.query.username}`);
+    navigator.clipboard.writeText(window.Location.hostname + router.query.username);
     toast.success('Copied to clipboard!', {
       position: 'bottom-right',
       theme: 'dark'
