cba2e62af606a9784f650bd62fb1a4a0935b02ce
Cole Sladowsky <coleslad31@gmail.com>
2026-02-09 10:21:21 -0500

    Increase body parser limit to 10mb for image uploads

--- server/index.js
+++ server/index.js
@@ -17,8 +17,9 @@ const { loadSocials, loadLinks } = require('./controllers/loadPrevious')
 
 
 
-app.use(cors()); 
-app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('mongodb connected')).catch((err) => console.log(err.message + " line 18"));
 
