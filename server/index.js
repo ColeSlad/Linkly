ebbaf2a7a553eea7fc5c899c371448b91ea02bae
Cole Sladowsky <coleslad31@gmail.com>
2026-03-02 15:36:05 -0500

    Add helmet security headers middleware

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const helmet = require('helmet');
 
 
 const mongoose = require('mongoose');
@@ -17,6 +18,7 @@ const { loadSocials, loadLinks } = require('./controllers/loadPrevious')
 
 
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
